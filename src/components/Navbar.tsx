f40652a3bd7c6eb9b487e4ed5137d6f79f2e8f80
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-03-08 01:15:43 +0530

    Added Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
+
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWIdthWrapper";
+import { Button, buttonVariants } from "./ui/button";
+import { SignInButton, SignUpButton, UserButton, currentUser } from "@clerk/nextjs"
+import { cn } from "@/lib/utils";
 
-const Navbar=()=>{
+
+
+const Navbar=async ()=>{
+    const user=await currentUser();
     return(
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -9,6 +16,28 @@ const Navbar=()=>{
                     <Link href='/' className="flex z-40 font-semibold">
                         <span>poki.</span>
                     </Link>
+                    <div className="hidden items-center space-x-4 sm:flex">
+                        <>
+                            <Link href='/pricing' className={buttonVariants({variant:"ghost",size:"sm"})}>Pricing</Link>
+                            {!user && (
+                                <>
+                                    <SignInButton>
+                                        <div className={cn(buttonVariants({size:"sm"}),"bg-black text-white cursor-pointer")}>Login</div>
+                                    </SignInButton>
+                                    <SignUpButton>
+                                        <div className={cn(buttonVariants({size:"sm"}),"bg-black text-white cursor-pointer")}>Get Started</div>
+                                    </SignUpButton>
+                                </>
+                            )}
+                            {!!user && (   
+                                <>
+                                    <span className="text-sm font-semibold text-red-500 capitalize">{`Hi ${user.firstName}`}</span>
+                                    <UserButton afterSignOutUrl="/"/>
+                                    <Link href='/dashboard' className={cn(buttonVariants({size:"sm"}),"bg-black text-white")}>Go To Dashboard</Link>
+                                </>
+                            )}
+                        </>
+                    </div>
                 </div>
             </MaxWidthWrapper>
         </nav>
